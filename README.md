c82600ba736008f9c0dfc4006685b81e7637e266
Marcos <mmaia@localhost.localdomain>
2015-12-11 23:30:16 +0100

    mongo db script done

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-Cria um container meanjs.org
- 
-Para criar uma imagem digite: 
+Apos instalar o ubuntu(testado no ubuntu 14.04) fazer um update para atualizar:
+
+{code}
+sudo apt-get update
+{code}
+
+eh instalar o git para poder clonar este repositorio.
+
+{code}
+sudo apt-get install git
+{code}
+
+Intalando o banco de dados nosql MongoDB ultima versao estavel
 
 
-Para rodar a imagem use:
-
-Exponha o diretório raiz onde você fez 
 
 
 
---
+++ install-mongodb-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# MongoDB installation
+#
+# Written by Marcos Maia <maia.marcos@gmail.com>
+#
+# This script install mongodb on ubuntu. Tested on ubuntu 14.04
+# It uses commands listed in official mongodb webpage - https://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+
+echo "Adding mongo db repository key"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "creating list file for ubuntu 14.04"
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+echo "updates ubuntu to recognize added mongodb repository"
+sudo apt-get update
+
+echo "installing latest stable version of mongodb packages"
+sudo apt-get install -y mongodb-org
+
+echo "starting mongodb"
+sudo service mongod start
