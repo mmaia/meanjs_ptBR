2d0488ee85d32298e8850e44d7b265e42d238315
mmaia <maia.marcos@gmail.com>
2015-12-13 23:02:29 +0100

    finish first version of script

--- install-nodejs-0.12.9.sh
+++ install-nodejs-0.12.9.sh
@@ -7,7 +7,7 @@ cd ~
 #download nodejs
 curl -O https://nodejs.org/dist/v0.12.9/node-v0.12.9-linux-x64.tar.gz
 
-#uncompress, delete compressed and move nodejs to /opt changing it's name to node-v0.12.9
+#uncompress, delete compressed and move nodejs to ~/tools changing it's name to node-v0.12.9
 tar -xvf node-v0.12.9-linux-x64.tar.gz
 rm -rf node-v0.12.9-linux-x64.tar.gz
 mkdir ~/tools
@@ -15,11 +15,10 @@ mv node-v0.12.9-linux-x64 ~/tools/node-v0.12.9
 
 #add node and npm to .bashrc
 echo 'export PATH=$PATH:~/tools/node-v0.12.9/bin:~/npm-global/bin' >> ~/.bashrc
-source .bashrc
 
 #change default nodejs repository to current user npm-global folder, see: https://docs.npmjs.com/getting-started/fixing-npm-permissions
 mkdir ~/npm-global
-npm config set prefix '~/npm-global'
+~/tools/node-v0.12.9/bin/npm config set prefix '~/npm-global'
 
 
 
